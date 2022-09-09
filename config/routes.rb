4d0002e130936155ea64cb42f189e2499f74af6a
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-09-09 08:45:37 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'book/new'
+  get 'book/index'
+  get 'book/show'
+  get 'book/edit'
+  get 'books/top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
