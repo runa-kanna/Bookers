2b5d4f3cc49a6825baf2242116c9153d96d8cfcc
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-09-11 02:45:38 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,6 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
   end
   
   def destroy
