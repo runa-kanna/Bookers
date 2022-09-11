d5018d93f419551dfff4ead99db289b1e03b2935
Ayaka <ec2-user@ip-172-31-4-30.ap-northeast-1.compute.internal>
2022-09-11 09:25:28 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,51 +1,51 @@
 class BooksController < ApplicationController
- 
+
   def new
     @book = Book.new
   end
-  
+
   def create
     @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
-    else
+    elsif
       render :index
-    end  
-    flash[:notice] = "Book was successfully created" 
+    end
+    flash[:notice] = "Book was successfully created"
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
-    
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    flash[:notice] = "Book was successfully updated" 
+    
+    flash[:notice] = "Book was successfully updated"
     redirect_to '/books'
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
-    flash[:notice] = "Book was successfully destroyed." 
+    flash[:notice] = "Book was successfully destroyed."
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root :to => 'homes#top'
   get '/top' => 'homes#top'
   resources :books
+
 
 end
